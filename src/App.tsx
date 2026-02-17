c8334f30f16bd6f8d67f670b3f4d0d03b9b09f95
william-schlegel <william.schlegel@gmail.com>
2026-02-17 14:45:20 +0100

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <section className="max-w-7xl mx-auto py-10">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">
+        Test Marble
+      </h1>
+      <p className="text-sm text-muted-foreground">
+        Composant de base pour la définition de règles.
+      </p>
+    </section>
+  );
+}
+
+export default App;
